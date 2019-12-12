abfe4001b5ac95e693eb72207fac89799dab0dd4
R Sánchez <rene.as.himself@gmail.com>
2019-12-12 11:00:59 -0600

    rest-auth with JWT on login

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -146,8 +146,8 @@ SITE_ID = 1
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
