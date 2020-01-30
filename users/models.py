eb52c773d71a1b8269cdc6b0c7afa62a48abafbc
R Sánchez <rene.as.himself@gmail.com>
2020-01-30 02:59:44 -0600

    Custom user model
    * Log in with email
    * UserList and UserDetail views

--- users/models.py
+++ users/models.py
@@ -1,7 +1,18 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class CustomUser(AbstractUser):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(
+        verbose_name="username", max_length=255, unique=True, null=True, blank=True
+    )
+    email = models.EmailField(verbose_name="email address", max_length=255, unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
     def __str__(self):
         return self.email
