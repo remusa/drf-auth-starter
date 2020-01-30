ce022e35d67080f6a4b8d8bc686fe656ab6bd73d
R Sánchez <rene.as.himself@gmail.com>
2020-01-30 03:00:59 -0600

    Updated main app urls.py

--- app/urls.py
+++ app/urls.py
@@ -13,19 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-
-# from rest_framework.schemas import get_schema_view
+from django.views.decorators.csrf import csrf_exempt
 
 from .views import api_root
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("allauth.urls")),
-    path("users/", include("users.urls")),
-    path("rest-auth/", include("rest_auth.urls")),
-    path("rest-auth/registration/", include("rest_auth.registration.urls")),
+    path("api/v1/", include("api.urls")),
+    # path("accounts/", include("api.urls")),
+    path("api-auth/", include("rest_framework.urls")),
     path("", api_root),
     # path(
     #     "openapi",
@@ -39,3 +38,8 @@ urlpatterns = [
     #     name="openapi-schema",
     # ),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
