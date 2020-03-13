2d79ba7e504d38ee245a824d4ce171b82599d78e
R Sánchez <rene.as.himself@gmail.com>
2020-03-13 13:57:16 -0600

    Added pagination

--- app/settings.py
+++ app/settings.py
@@ -166,6 +166,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 AUTHENTICATION_BACKENDS = (
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics, viewsets
-from rest_framework.permissions import IsAdminUser
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
 from .serializers import UserSerializer
@@ -11,4 +13,7 @@ CustomUser = get_user_model()
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated, IsAdminUser]
     permission_classes = [IsAdminUser]
+    pagination_classes = [PageNumberPagination]
